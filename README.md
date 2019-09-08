29bc9f85acefd0341a7ffa11e69105416fb2a0a3
Duy Lai <duyklai@gmail.com>
2019-09-08 00:12:39 -0500

    Added button functionality; added sounds; updated readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Pomodoro Clock
 Quick website for Pomodoro Technique to help studying/procastination.
-Currently only supports up to 1 hour of studying and break time.
+Currently only supports up to 1 hour of work and break time. Currently will play the alarm after rest timer has counted down to require user to reset and start timer again when both time runs down.
+
+Both alarm sounds are pleasant as to not irritate user.
 
 Utilized HTML, CSS, and JS
--- resources/script.js
+++ resources/script.js
@@ -1,60 +1,172 @@
+//Script for pomodoro-clock
 const workDisplay = document.querySelector('#workDisplay');
 const restDisplay = document.querySelector('#restDisplay');
+var workSelected, restSelected;
+var workTimer, restTimer;
+var workDone = false;
+var timerStarted = false;
+var breakSound = new Audio('resources/breakTime.mp3');
+breakSound.volume(0.8);
+var workSound = new Audio('resources/workTime.mp3');
+workSound.volume(0.8);
 
-const show = function(min)
+const startTimer = function()
 {
-    // Setting up two variables for current and x minutes away
-    var currentTime = new Date().getTime();
-    var countDown = new Date();
-    countDown.setTime(currentTime + (min * 60 * 1000));
-
-    var x = setInterval(function() 
+    // If check to disable "start" button spam
+    if(!timerStarted)
     {
-        // Update time
-        currentTime = new Date().getTime();
+        // Set boolean check for collecting and changing times
+        timerStarted = true;
+        // Set the timeSelected variable to the selected count down time
+        getTimeSelected();
         
-        // Finding the difference
-        var diff = countDown - currentTime;
-        // Time calculations for days, hours, minutes and seconds
-        var minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((diff % (1000 * 60)) / 1000);
-        
-        // Output the result in an element with id="demo"
-        workDisplay.innerHTML = minutes + ":" + seconds;
-        
-        // If the count down is over, write some text 
-        if (diff < 0) 
+        // Setting up two variables for current and x minutes away
+        var currentTime = new Date().getTime();
+        var countDown = new Date();
+        countDown.setTime(currentTime + (workSelected * 60 * 1000));
+
+        workTimer = setInterval(function() 
         {
-            clearInterval(x);
-            workDisplay.innerHTML = "00:00";
-        }
-    }, 1000);
-}
+            // Update time
+            currentTime = new Date().getTime();
+            
+            // Finding the difference
+            var diff = countDown - currentTime;
 
-const reset = function()
-{
-    
-}
+            // Time calculations for minutes and seconds
+            var minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((diff % (1000 * 60)) / 1000);
+            seconds = ("0" + seconds).slice(-2);
+            
+            // Output the result on Work Time display
+            workDisplay.innerHTML = minutes + ":" + seconds;
+            
+            // If the count down is over
+            if (diff < 0) 
+            {
+                clearInterval(workTimer);
+                workDisplay.innerHTML = "0:00";
+                workDone = true;
+                breakSound.play();
 
-const increaseTime = function()
-{
-    var minutes = workDisplay.innerHTML.substr(0,2);
-    if(+(minutes) < 60)
-    {
-        minutes = +(minutes) + 1;
-        workDisplay.innerHTML = minutes + ":00";
+                // Getting new time information for rest time
+                currentTime = new Date().getTime();
+                countDown = new Date();
+                countDown.setTime(currentTime + (restSelected * 60 * 1000));
+
+                // Start rest timer
+                restTimer = setInterval(function() 
+                {
+                    // Update time
+                    currentTime = new Date().getTime();
+                    
+                    // Finding the difference
+                    var diff = countDown - currentTime;
+
+                    // Time calculations for minutes and seconds
+                    var minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+                    var seconds = Math.floor((diff % (1000 * 60)) / 1000);
+                    seconds = ("0" + seconds).slice(-2);
+                    
+                    // Output the result on Rest Time display
+                    restDisplay.innerHTML = minutes + ":" + seconds;
+                    
+                    // If the count down is over
+                    if (diff < 0) 
+                    {
+                        clearInterval(workTimer);
+                        restDisplay.innerHTML = "0:00";
+                        workDone = false;
+                        workSound.play();
+                    }
+                }, 1000);
+            }
+        }, 1000);
     }
 }
 
-const decreaseTime = function()
+// Getting the selected time from user, putting them into variable
+// Will only be ran once the start button has been pushed
+const getTimeSelected = function()
 {
     var tempLength = workDisplay.innerHTML.length;
-    var minutes = workDisplay.innerHTML.substr(0,tempLength-3);
-    if(+(minutes) > 0)
+    workSelected = +(workDisplay.innerHTML.substr(0, tempLength - 3));
+    tempLength = restDisplay.innerHTML.length;
+    restSelected = +(restDisplay.innerHTML.substr(0, tempLength - 3));
+}
+
+// Reset display, timers, and booleans for checks
+const reset = function()
+{
+    clearInterval(workTimer);
+    clearInterval(restTimer);
+    timerStarted = false;
+    workDone = false;
+    workDisplay.innerHTML = workSelected + ":00";
+    restDisplay.innerHTML = restSelected + ":00";
+}
+
+// Initial function for determining change of time; passed in incr or decr and the button element itself
+const changeTime = function(change, e)
+{
+    // If we want to increase time
+    if(change == "up")
     {
-        minutes = +(minutes) - 1;
-        workDisplay.innerHTML = minutes + ":00";
+        // Find out from which time div
+        if(e.parentNode.id.includes("rest"))
+        {
+            increaseTime(restDisplay);
+        }
+        else if(e.parentNode.id.includes("work"))
+        {
+            increaseTime(workDisplay);
+        }
+    }
+    else if(change == "down")   // If we want to decrease time
+    {
+        // Find out from which time div
+        if(e.parentNode.id.includes("rest"))
+        {
+            decreaseTime(restDisplay);
+        }
+        else if(e.parentNode.id.includes("work"))
+        {
+            decreaseTime(workDisplay);
+        }
     }
 }
+
+// Function for increasing the time on the display
+const increaseTime = function(disp)
+{
+    // Make sure display cannot be changed once timer has started
+    if(!timerStarted)
+    {
+        var tempLength = disp.innerHTML.length;
+        var minutes = disp.innerHTML.substr(0, tempLength - 3);
+        if(+(minutes) < 60)
+        {
+            minutes = +(minutes) + 1;
+            disp.innerHTML = minutes + ":00";
+        }
+    }
+}
+
+// Function for decrease the time on the display
+const decreaseTime = function(disp)
+{
+    // Make sure display cannot be changed once timer has started
+    if(!timerStarted)
+    {
+        var tempLength = disp.innerHTML.length;
+        var minutes = disp.innerHTML.substr(0, tempLength - 3);
+        if(+(minutes) > 1)
+        {
+            minutes = +(minutes) - 1;
+            disp.innerHTML = minutes + ":00";
+        }
+    }
+}
+
 
 
